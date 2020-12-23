ecb258d7a6177cf727cb31675e893427b5dbcac3
ekhusainov <xluttiy@gmail.com>
2020-12-24 02:25:57 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,31 @@
 import joblib
+import os
 
-vectorizer = joblib.load("news_vectorizer_dump.pkl")
-our_model = joblib.load("news_model_dump.pkl")
+DIRNAME = os.path.dirname(__file__)
 
-class_list = ['action', 'adventure', 'animation', 'biography', 'comedy',
+CLASS_LIST = ['action', 'adventure', 'animation', 'biography', 'comedy',
               'crime', 'drama', 'family', 'fantasy', 'history', 'horror',
               'music', 'musical', 'mystery', 'romance', 'sci-fi', 'thriller',
               'war']
 
-our_text = input()
 
-class_index = our_model.predict(vectorizer.transform(our_text))[0]
-print(class_list[class_index])
+def get_pkl(filepath):
+    filename = os.path.join(DIRNAME, filepath)
+    return joblib.load(filename)
+
+# filename = os.path.join(dirname, 'news_vectorizer_dump.pkl')
+# vectorizer = joblib.load(filename)
+# filename = os.path.join(dirname, 'news_model_dump.pkl')
+# our_model = joblib.load(filename)
+
+
+def main():
+    vectorizer = get_pkl('news_vectorizer_dump.pkl')
+    our_model = get_pkl('news_model_dump.pkl')
+    our_text = [input()]
+    class_index = our_model.predict(vectorizer.transform(our_text))[0]
+    print(CLASS_LIST[class_index])
+
+
+if __name__ == "__main__":
+    main()
