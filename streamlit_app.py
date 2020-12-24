6a85b390016dcb1d27497f9dabe5dc4f73e44f83
ekhusainov <xluttiy@gmail.com>
2020-12-24 21:30:51 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -54,7 +54,8 @@ def main():
     # st.text(f"Genre: {answer}")
     st.text("Current situation:")
     if class_index:
-        st.text("ALARM!!!")
+        st.markdown("<h2 style='text-align: center; color: red;'>ALARM!!!</h2>")
+        # st.text("ALARM!!!")
     else:
         st.text("Normal")
 
