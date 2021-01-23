774c8aed73c07793645900fce5c5493fd4a3e4a4
ekhusainov <xluttiy@gmail.com>
2021-01-24 01:17:02 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -47,6 +47,20 @@ def tranform_our_string(our_string):
     our_string = ' '.join(our_string)
     return our_string
 
+def transform_text(our_text):
+    our_text = tranform_our_string(our_text)
+
+    class_index = our_model.predict(vectorizer.transform([our_text]))[0]
+    st.text("Current situation:")
+
+    if class_index:
+        st.markdown("<h2 style='text-align: center; color: red;'>ALARM!!!</h2>",
+                    unsafe_allow_html=True)
+
+    else:
+        st.markdown("<h2 style='text-align: center; color: green;'>Normal situation</h2>",
+                    unsafe_allow_html=True)
+
 
 def main():
     vectorizer = get_pkl('news_vectorizer_dump_0_1.pkl')
@@ -62,22 +76,24 @@ def main():
         our_text = EXAMPLE_TEXT[random_index]
         st.text_input(TEXT_FROM_CURRENT_PERSON,
                       value=our_text)
+        transform_text(our_text)
     else:
         our_text = st.text_input(TEXT_FROM_CURRENT_PERSON)
+        transform_text(our_text)
         
 
-    our_text = tranform_our_string(our_text)
+    # our_text = tranform_our_string(our_text)
 
-    class_index = our_model.predict(vectorizer.transform([our_text]))[0]
-    st.text("Current situation:")
+    # class_index = our_model.predict(vectorizer.transform([our_text]))[0]
+    # st.text("Current situation:")
 
-    if class_index:
-        st.markdown("<h2 style='text-align: center; color: red;'>ALARM!!!</h2>",
-                    unsafe_allow_html=True)
+    # if class_index:
+    #     st.markdown("<h2 style='text-align: center; color: red;'>ALARM!!!</h2>",
+    #                 unsafe_allow_html=True)
 
-    else:
-        st.markdown("<h2 style='text-align: center; color: green;'>Normal situation</h2>",
-                    unsafe_allow_html=True)
+    # else:
+    #     st.markdown("<h2 style='text-align: center; color: green;'>Normal situation</h2>",
+    #                 unsafe_allow_html=True)
 
 
 if __name__ == "__main__":
